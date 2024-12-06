8c4fb4ae56d98d24882cc0b729078b0d17cae811
k.kozlowski <k.kozlowski@reply.com>
2024-12-06 14:52:41 +0100

    spring

---
+++ src/main/java/photos/kozlowski/aws_rekognition/AwsRekognitionApplication.java
@@ -0,0 +1,13 @@
+package photos.kozlowski.aws_rekognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsRekognitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsRekognitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/photos/kozlowski/aws_rekognition/AwsRekognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package photos.kozlowski.aws_rekognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsRekognitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
